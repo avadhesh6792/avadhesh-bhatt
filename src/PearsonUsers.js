d0f18f1e0d04c17ae168b221a23a3cbcbc08ebf5
Avadhesh <avadheshbhatt92@gmail.com>
2018-09-22 23:04:05 +0530

    working on UI

--- src/PearsonUsers.js
+++ src/PearsonUsers.js
@@ -1,27 +1,30 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export class PearsonUsers extends Component {
   constructor(props) {
     super(props);
+    this.displayUsers = this.displayUsers.bind(this);
+    this.deleteUser = this.deleteUser.bind(this);
 
     this.state = {
       users: [
         {
-          id: 4,
+          id: 14,
           first_name: "Eve",
           last_name: "Holt",
           avatar:
             "https://s3.amazonaws.com/uifaces/faces/twitter/marcoramires/128.jpg"
         },
         {
-          id: 5,
+          id: 15,
           first_name: "Charles",
           last_name: "Morris",
           avatar:
             "https://s3.amazonaws.com/uifaces/faces/twitter/stephenmoon/128.jpg"
         },
         {
-          id: 6,
+          id: 16,
           first_name: "Tracey",
           last_name: "Ramos",
           avatar:
@@ -31,11 +34,50 @@ export class PearsonUsers extends Component {
     };
   }
 
+  componentDidMount(){
+    axios.get('https://reqres.in/api/users?page=1&per_page=10')
+      .then( response =>  {
+        let users = [ ...this.state.users, ...response.data.data];
+        this.setState({users});
+    })
+      .catch(function (error) {
+        console.log(error);
+    });
+  }
+
+  displayUsers(){
+    let { users } = this.state;
+    return (
+      <ul className="list container clearfix">
+          {
+            users.map(user => <li className="item" key={user.id}>
+              <div className="avatar-wrapper">
+                <img src={user.avatar} className="avatar" alt={`${user.first_name} ${user.last_name}`} />
+              </div>
+              <div className="full-name-wrapper">
+                  <span>{`${user.first_name} ${user.last_name}`}</span>
+              </div>
+              <button className="delete-user-btn" onClick={e => this.deleteUser(user.id)}>Delete</button>
+            </li>)
+          }
+        </ul>
+    );
+  }
+
+  deleteUser(userId){
+    let { users } = this.state;
+    var index = users.findIndex( element => element.id === userId);
+    users.splice(index, 1);
+    this.setState({ users });
+  }
+
   render() {
     return (
-      <div className="pearon-users">
-        <h1>Pearson User Management</h1>
-        {/* Render users here */}
+      <div className="pearson-users">
+        <h1 className="container heading">Pearson User Management</h1>
+        <div className="list-wrapper">
+          {this.displayUsers()}
+        </div>
       </div>
     );
   }
